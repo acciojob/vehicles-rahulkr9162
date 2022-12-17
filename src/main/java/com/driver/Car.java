21dc3118cfb50c0e3fdba050f9e39d579c781667
rahulkr9162 <rahulkr9162@gmail.com>
2022-12-17 21:36:35 +0530

    me

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -58,7 +58,7 @@ public class Car extends Vehicle {
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        move(newSpeed, newDirection);
+        super.move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
